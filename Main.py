b4352b78189add1408a10dde0349487e45358e43
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2023-06-29 14:02:33 -0700

    run through all of them, print results, add angles

--- Main.py
+++ Main.py
@@ -9,35 +9,39 @@ import SimpleITK as sitk
 
 PDD = {'15X': {"Depth": [10.0191, 3.99859], "PDD": [76.6987, 96.1042]},
        '6X': {'Depth': [9.99814, 4.01534], "PDD": [69.5299, 90.9968]}}
-energy = '15X'
-path = fr"K:\DosePlane\{energy}"
-cross_plane_field_size = 200*.8  # in mm
-physical_start = cross_plane_field_size/2
-hard_shift = 0
-data = PDD[energy]
-u = math.log(data["PDD"][0]/data["PDD"][1])/(data["Depth"][1]-data["Depth"][0])
-if not os.path.exists(f"DosePlane_{energy}.nii.gz") or True:
-    Dicom_reader = DicomReaderWriter()
-    Dicom_reader.walk_through_folders(path)
-    Dicom_reader.get_dose()
-    dose_plane = Dicom_reader.dose_handle[:, :, 0]
-    sitk.WriteImage(dose_plane, f"DosePlane_{energy}.nii.gz")
-else:
-    dose_plane = sitk.ReadImage(f"DosePlane_{energy}.nii.gz")
+for energy in ['15X', '6X']:
+    for angle in [30, 60]:
+        path = fr"K:\DosePlane\{energy}\{angle}Degree"
+        file_name = f"DosePlane_{energy}_{angle}Degree.nii.gz"
+        cross_plane_field_size = 200*.8  # in mm
+        physical_start = cross_plane_field_size/2
+        hard_shift = 0
+        data = PDD[energy]
+        u = math.log(data["PDD"][0]/data["PDD"][1])/(data["Depth"][1]-data["Depth"][0])
+        if not os.path.exists(file_name) or True:
+            Dicom_reader = DicomReaderWriter(verbose=False)
+            Dicom_reader.walk_through_folders(path)
+            Dicom_reader.get_dose()
+            dose_plane = Dicom_reader.dose_handle[:, :, 0]
+            sitk.WriteImage(dose_plane, file_name)
+        else:
+            dose_plane = sitk.ReadImage(file_name)
 
-plane_size = dose_plane.GetSize()
-dose_plane_array = sitk.GetArrayFromImage(dose_plane)
-physical_center = dose_plane.TransformContinuousIndexToPhysicalPoint((plane_size[0]//2, plane_size[1]//2))
+        plane_size = dose_plane.GetSize()
+        dose_plane_array = sitk.GetArrayFromImage(dose_plane)
+        physical_center = dose_plane.TransformContinuousIndexToPhysicalPoint((plane_size[0]//2, plane_size[1]//2))
 
-wanted_start = dose_plane.TransformPhysicalPointToIndex((physical_center[0], physical_center[1] - physical_start))
-wanted_stop = dose_plane.TransformPhysicalPointToIndex((physical_center[0], physical_center[1] + physical_start))
-dose_line = dose_plane_array[wanted_start[1]:wanted_stop[1], wanted_start[0]]
-start_value = dose_line[0]*100  # in cGy
-stop_value = dose_line[-1]*100  # in cGy
-"""
-Now measuring the angle
-"""
-measured_angle = np.round(math.degrees(math.atan(math.log(start_value/stop_value)/(u*(physical_start*2/10)))), 2)
-above = np.round(math.degrees(math.atan(math.log(start_value*1.02/(stop_value*.98))/(u*(physical_start*2/10)))), 2)
-below = np.round(math.degrees(math.atan(math.log(start_value*0.98/(stop_value*1.02))/(u*(physical_start*2/10)))), 2)
-print(f"Measured angle is expected to be {measured_angle} and between {below} and {above}")
+        wanted_start = dose_plane.TransformPhysicalPointToIndex((physical_center[0],
+                                                                 physical_center[1] - physical_start))
+        wanted_stop = dose_plane.TransformPhysicalPointToIndex((physical_center[0],
+                                                                physical_center[1] + physical_start))
+        dose_line = dose_plane_array[wanted_start[1]:wanted_stop[1], wanted_start[0]]
+        start_value = dose_line[0]*100  # in cGy
+        stop_value = dose_line[-1]*100  # in cGy
+        """
+        Now measuring the angle
+        """
+        measured_angle = np.round(math.degrees(math.atan(math.log(start_value/stop_value)/(u*(physical_start*2/10)))), 2)
+        above = np.round(math.degrees(math.atan(math.log(start_value*1.02/(stop_value*.98))/(u*(physical_start*2/10)))), 2)
+        below = np.round(math.degrees(math.atan(math.log(start_value*0.98/(stop_value*1.02))/(u*(physical_start*2/10)))), 2)
+        print(f"For {energy} and {angle} angle, we expect it to be {measured_angle} and between {below} and {above}")
