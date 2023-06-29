b3a52614446d60c265e60fc5991a78a577fbffec
Brian Mark Anderson <b5anderson@health.ucsd.edu>
2023-06-29 11:06:02 -0700

    cleaning up

--- Main.py
+++ Main.py
@@ -5,7 +5,6 @@ import numpy as np
 from Dicom_RT_and_Images_to_Mask.src.DicomRTTool.ReaderWriter import DicomReaderWriter
 from PlotScrollNumpyArrays.Plot_Scroll_Images import plot_scroll_Image
 import SimpleITK as sitk
-from NiftiResampler.ResampleTools import ImageResampler
 
 
 PDD = {'15X': {"Depth": [10.0191, 3.99859], "PDD": [76.6987, 96.1042]},
@@ -18,10 +17,8 @@ hard_shift = 0
 data = PDD[energy]
 u = math.log(data["PDD"][0]/data["PDD"][1])/(data["Depth"][1]-data["Depth"][0])
 if not os.path.exists(f"DosePlane_{energy}.nii.gz") or True:
-    Dicom_reader = DicomReaderWriter(description='Examples', verbose=False, get_dose_output=True, Contour_Names=["Body"])
-    Dicom_reader.set_contour_names_and_associations(contour_names=["Body"])
+    Dicom_reader = DicomReaderWriter()
     Dicom_reader.walk_through_folders(path)
-    Dicom_reader.get_images()
     Dicom_reader.get_dose()
     dose_plane = Dicom_reader.dose_handle[:, :, 0]
     sitk.WriteImage(dose_plane, f"DosePlane_{energy}.nii.gz")
@@ -30,8 +27,8 @@ else:
 
 plane_size = dose_plane.GetSize()
 dose_plane_array = sitk.GetArrayFromImage(dose_plane)
-spacing = dose_plane.GetSpacing()[1]
 physical_center = dose_plane.TransformContinuousIndexToPhysicalPoint((plane_size[0]//2, plane_size[1]//2))
+
 wanted_start = dose_plane.TransformPhysicalPointToIndex((physical_center[0], physical_center[1] - physical_start))
 wanted_stop = dose_plane.TransformPhysicalPointToIndex((physical_center[0], physical_center[1] + physical_start))
 dose_line = dose_plane_array[wanted_start[1]:wanted_stop[1], wanted_start[0]]
